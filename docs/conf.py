715f3881c11724d91eba7721bfb9786a80d04216
Jens Meinecke <1739138+meinjens@users.noreply.github.com>
2021-03-23 09:34:09 +0100

    doc: mehl mehl mehl

--- docs/conf.py
+++ docs/conf.py
@@ -8,18 +8,18 @@ extensions = ['sphinx.ext.imgmath', 'sphinx.ext.viewcode']
 templates_path = ['_templates']
 source_suffix = '.rst'
 master_doc = 'index'
-project = 'Mailu'
-copyright = '2018, Mailu authors'
-author = 'Mailu authors'
+project = 'Mehl'
+copyright = '2021, Mehl authors'
+author = 'Mehl authors'
 version = release = os.environ.get('VERSION', 'master')
 language = None
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store', 'Dockerfile', 'docker-compose.yml']
 pygments_style = 'sphinx'
 todo_include_todos = False
 html_theme = 'sphinx_rtd_theme'
-html_title = 'Mailu, Docker based mail server'
+html_title = 'Mehl, Docker based mail server'
 html_static_path = []
-htmlhelp_basename = 'Mailudoc'
+htmlhelp_basename = 'Mehldoc'
 
 # Custom sidebar templates, must be a dictionary that maps document names
 # to template names.
